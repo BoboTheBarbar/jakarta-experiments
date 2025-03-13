f379c3001f8cc1d2ee85cffbe813e38aa321c150
Jannis Mühlbach <jannis.muehlbach@colenio.com>
2025-03-13 13:21:44 +0100

    test: add tests for Component

--- JakartaEEHelloWorld2/src/test/java/com/colenio/jakartaeehelloworld2/arquillianexperiments/ComponentTest.java
+++ JakartaEEHelloWorld2/src/test/java/com/colenio/jakartaeehelloworld2/arquillianexperiments/ComponentTest.java
@@ -1,23 +1,43 @@
 package com.colenio.jakartaeehelloworld2.arquillianexperiments;
 
+import jakarta.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.container.annotation.ArquillianTest;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 @ArquillianTest
 class ComponentTest {
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addPackages(true, "org.wildfly.guide.testing")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
+    @Inject
+    Component component;
+
     @Test
-    void test() {
-        assert true;
+    void testMessage() {
+        String message = "message";
+        Assertions.assertEquals("Message, " + message, component.message(message));
+    }
+
+    @Test
+    void testSendMessage() {
+        String message = "message";
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        component.sendMessage(printStream, message);
+
+        Assertions.assertEquals(message, outputStream.toString());
     }
 }
